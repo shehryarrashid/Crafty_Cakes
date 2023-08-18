ef1689d644aff2a72cd66940f063cfde46f1d032
Shehryar Rashid <94257069+shehryarrashid@users.noreply.github.com>
2023-08-18 20:20:24 +0100

    UserData

--- src/Assignment/Database/userData.java
+++ src/Assignment/Database/userData.java
@@ -2,7 +2,7 @@ package Assignment.Database;
 
 public class userData {
     private String username = "root";
-    private String password = "All@hisgreat2002";
+    private String password = "yourPassword";
     private String url = "jdbc:mysql://127.0.0.1:3306/Crafty_Cakes?allowMultiQueries=true";
 
     public String getPassword() {
